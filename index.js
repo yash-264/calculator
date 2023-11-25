ba1359274bfb5bb27aca96292755505ec07244f2
yash-264 <150003197+yash-264@users.noreply.github.com>
2023-11-25 08:00:58 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+let string="";
+let buttons=document.querySelectorAll("button")
+
+buttons.forEach((button)=>{
+
+    button.addEventListener('click',(e)=>{
+
+        if(e.target.innerHTML=="="){
+           
+            string=eval(string);
+            document.querySelector("input").value=string
+
+        }
+        else if(e.target.innerHTML=="Ac"){
+          
+            string=""
+            document.querySelector("input").value=string;
+        }
+        else if(e.target.innerHTML=="Del"){
+          
+            string="";
+            document.querySelector("input").value=string;
+
+        }
+        else if(e.target.innerHTML=="ON"){
+            
+           string=""
+           document.querySelector("input").value=string
+
+        }
+        else{
+            console.log(e.target)
+            string=string+e.target.innerHTML;
+            document.querySelector("input").value=string;
+        }
+
+    })
+    
+})
